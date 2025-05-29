e0a5ccf1141090358c6483c285c9fa4d715a9b62
uav_8 <uav_8@uav8ing>
2025-05-29 16:39:05 +0100

    Version2 of IMU calibration after tests.

---
+++ src/imu_calibration_v2.cpp
@@ -0,0 +1,70 @@
+#include <ros/ros.h>
+#include <sensor_msgs/Imu.h>
+#include <geometry_msgs/QuaternionStamped.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/sync_policies/approximate_time.h>
+#include <message_filters/synchronizer.h>
+
+using namespace sensor_msgs;
+using namespace message_filters;
+
+class ImuMerger
+{
+public:
+    ImuMerger()
+    {
+        nh = ros::NodeHandle("~");
+        imu_a_sub_.subscribe(nh, "input_topic_main", 10);
+        imu_b_sub_.subscribe(nh, "input_topic_ref", 10);
+
+        sync_.reset(new Synchronizer<SyncPolicy>(SyncPolicy(10), imu_a_sub_, imu_b_sub_));
+        sync_->registerCallback(boost::bind(&ImuMerger::callback, this, _1, _2));
+
+        imu_pub_ = nh.advertise<Imu>("output_topic_imu", 10);
+        orientation_pub_ = nh.advertise<geometry_msgs::QuaternionStamped>("output_topic_orientation", 1);
+    }
+
+private:
+    typedef sync_policies::ApproximateTime<Imu, Imu> SyncPolicy;
+
+    ros::NodeHandle nh;
+    message_filters::Subscriber<Imu> imu_a_sub_;
+    message_filters::Subscriber<Imu> imu_b_sub_;
+    boost::shared_ptr<Synchronizer<SyncPolicy>> sync_;
+    ros::Publisher imu_pub_, orientation_pub_;
+
+    void callback(const ImuConstPtr& imu_a, const ImuConstPtr& imu_b)
+    {
+        Imu merged;
+        geometry_msgs::QuaternionStamped q_msg;
+
+        // Use header from IMU A
+        merged.header = imu_a->header;
+
+        // Orientation from IMU B
+        merged.orientation = imu_b->orientation;
+        merged.orientation_covariance = imu_b->orientation_covariance;
+
+        // Angular velocity and acceleration from IMU A
+        merged.angular_velocity = imu_b->angular_velocity;
+        merged.angular_velocity_covariance = imu_b->angular_velocity_covariance;
+
+        merged.linear_acceleration = imu_b->linear_acceleration;
+        merged.linear_acceleration_covariance = imu_b->linear_acceleration_covariance;
+
+        imu_pub_.publish(merged);
+        q_msg.header = merged.header;
+        q_msg.quaternion = merged.orientation;
+        orientation_pub_.publish(q_msg);
+    }
+};
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "imu_merger_node");
+
+    ImuMerger merger;
+
+    ros::spin();
+    return 0;
+}
