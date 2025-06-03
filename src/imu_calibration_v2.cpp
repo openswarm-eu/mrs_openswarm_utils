09b0bf9b2b273315051e79ffa40bd9b4f5eb8006
Carlos <carlos@ingeniarius.pt>
2025-06-03 14:18:46 +0100

    Add wit calibrator to version 2 of imu_cal.

--- src/imu_calibration_v2.cpp
+++ src/imu_calibration_v2.cpp
@@ -1,4 +1,5 @@
 #include <ros/ros.h>
+#include <tf/tf.h>
 #include <sensor_msgs/Imu.h>
 #include <geometry_msgs/QuaternionStamped.h>
 #include <message_filters/subscriber.h>
@@ -14,7 +15,10 @@ public:
     ImuMerger()
     {
         nh = ros::NodeHandle("~");
+        nh.param<int>("maxSize", maxSize, 1000);
         nh.param<std::string>("uav_name", uav_name, "uav6");
+        nh.param<bool>("imu_offset", imu_offset, false);
+        nh.param<double>("wit_offset", wit_offset, 0.0);
         imu_a_sub_.subscribe(nh, "input_topic_main", 10);
         imu_b_sub_.subscribe(nh, "input_topic_ref", 10);
 
@@ -23,6 +27,37 @@ public:
 
         imu_pub_ = nh.advertise<Imu>("output_topic_imu", 10);
         orientation_pub_ = nh.advertise<geometry_msgs::QuaternionStamped>("output_topic_orientation", 1);
+    }
+
+    // Calculate the average of the current elements in the buffer
+    double calculateAverage(std::deque<tf::Quaternion> buffer)
+    {
+        double roll, pitch, heading;
+        double sum = 0.0;
+        int index = 0;
+
+        while (!buffer.empty())
+        {
+            tf::Matrix3x3(buffer.front()).getRPY(roll,pitch,heading);
+            sum += heading;
+            buffer.pop_front();
+            index += 1;
+        }
+        return sum / index;
+
+    }
+
+    template<typename T>
+    void imuRPY2rosRPY(sensor_msgs::Imu *thisImuMsg, T *rosRoll, T *rosPitch, T *rosYaw)
+    {
+        double imuRoll, imuPitch, imuYaw;
+        tf::Quaternion orientation;
+        tf::quaternionMsgToTF(thisImuMsg->orientation, orientation);
+        tf::Matrix3x3(orientation).getRPY(imuRoll, imuPitch, imuYaw);
+
+        *rosRoll = imuRoll;
+        *rosPitch = imuPitch;
+        *rosYaw = imuYaw;
     }
 
 private:
@@ -34,9 +69,34 @@ private:
     boost::shared_ptr<Synchronizer<SyncPolicy>> sync_;
     ros::Publisher imu_pub_, orientation_pub_;
     std::string uav_name;
+    double heading_imu, wit_offset;
+    bool imu_offset;
+    std::deque<tf::Quaternion> buffer2_;
+    int maxSize;
 
     void callback(const ImuConstPtr& imu_a, const ImuConstPtr& imu_b)
     {
+        ROS_INFO_ONCE("Callback function for the WIT IMU sensor.");
+        sensor_msgs::Imu imu_data1_ = *imu_b;
+
+        if (buffer2_.size() < maxSize)
+        {
+            ROS_INFO_ONCE("Buffering...");
+            tf::Quaternion RQ2 = {imu_b->orientation.x,imu_b->orientation.y,imu_b->orientation.z,imu_b->orientation.w};
+            buffer2_.push_back(RQ2);
+            return;
+        }
+
+        static bool initial_heading = false;
+        if (!initial_heading)
+        {
+            initial_heading = true;
+            ROS_INFO_ONCE("Calculate the average of the current elements in the buffer.");
+            heading_imu = calculateAverage(buffer2_);
+            ROS_INFO_ONCE("Offset from WIT: %f", heading_imu);
+            return;
+        }
+
         Imu merged;
         geometry_msgs::QuaternionStamped q_msg;
 
@@ -48,6 +108,13 @@ private:
         // Orientation from IMU B
         merged.orientation = imu_b->orientation;
         merged.orientation_covariance = imu_b->orientation_covariance;
+
+        double roll, pitch, heading;
+        imuRPY2rosRPY(&imu_data1_, &roll, &pitch, &heading);
+        if (imu_offset)
+        {
+            merged.orientation = tf::createQuaternionMsgFromRollPitchYaw(roll, pitch, heading - wit_offset);
+        }
 
         // Angular velocity and acceleration from IMU A
         merged.angular_velocity = imu_b->angular_velocity;
